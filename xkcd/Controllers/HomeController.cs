d79db2b8b9a0586c1afbca9e0f0679fe627bba48
larry.navarrete <larry>
2019-04-05 17:41:19 -0600

    Linea base

---
+++ xkcd/Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+using xkcd.ViewModels;
+
+namespace xkcd.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string url = "https://xkcd.com/info.0.json";
+            //string url = "https://xkcd.com/404/info.0.json";
+            using (HttpClient client = new HttpClient())
+            {
+                //https://xkcd.com/404/info.0.json
+                //https://xkcd.com/info.0.json
+                //var xkcdJsonData = client.DownloadString("https://xkcd.com/info.0.json");
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("aplication/json"));
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var xkcdJsonData = response.Content.ReadAsStringAsync().Result;
+                    ComicOfTheDay data = JsonConvert.DeserializeObject<ComicOfTheDay>(xkcdJsonData);
+                    data.prev = data.num - 1;
+                    data.next = data.num + 1;
+                    return View(data);
+                }
+                else
+                {
+                    return View();
+                }
+
+            }
+            
+        }
+
+        public ActionResult SpecifiComic(int comicId)
+        {
+            //string url = "https://xkcd.com/info.0.json";
+            string url = "https://xkcd.com/"+comicId+"/info.0.json";
+            using (HttpClient client = new HttpClient())
+            {
+                //https://xkcd.com/404/info.0.json
+                //https://xkcd.com/info.0.json
+                //var xkcdJsonData = client.DownloadString("https://xkcd.com/info.0.json");
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("aplication/json"));
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var xkcdJsonData = response.Content.ReadAsStringAsync().Result;
+                    ComicOfTheDay data = JsonConvert.DeserializeObject<ComicOfTheDay>(xkcdJsonData);
+                    data.prev = data.num - 1;
+                    data.next = data.num + 1;
+
+
+                    if (!client.GetAsync("https://xkcd.com/" + data.prev + "/info.0.json").Result.IsSuccessStatusCode)
+                    {
+                        data.prev--;
+                    }
+
+                    if (!client.GetAsync("https://xkcd.com/" + data.next + "/info.0.json").Result.IsSuccessStatusCode)
+                    {
+                        data.next++;
+                    }
+                    
+
+
+                    return View(data);
+                }
+                else
+                {
+                    return Index();
+                }
+
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        
+    }
+}
---
+++ xkcd/ViewModels/ComicOfTheDay.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace xkcd.ViewModels
+{
+    public class ComicOfTheDay
+    {
+            public string month { get; set; }
+            public int num { get; set; }
+            public string link { get; set; }
+            public string year { get; set; }
+            public string news { get; set; }
+            public string safe_title { get; set; }
+            public string transcript { get; set; }
+            public string alt { get; set; }
+            public string img { get; set; }
+            public string title { get; set; }
+            public string day { get; set; }
+            public int? prev { get; set; }
+            public int? next { get; set; }
+
+
+
+    }
+
+    //{
+    //"month": "1",
+    //"num": 3,
+    //"link": "",
+    //"year": "2006",
+    //"news": "",
+    //"safe_title": "Island (sketch)",
+    //"transcript": "[[A sketch of an Island]]\n{{Alt:Hello, island}}",
+    //"alt": "Hello, island",
+    //"img": "https://imgs.xkcd.com/comics/island_color.jpg",
+    //"title": "Island (sketch)",
+    //"day": "1"
+    //}
+}
